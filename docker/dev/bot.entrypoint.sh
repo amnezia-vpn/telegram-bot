160cede2b588c1dda9c2794aaf6e7eb8fb672b54
Dmitry Danilov <dima.danilov9867@gmail.com>
2022-11-03 16:47:58 +0400

    Fix gunicorn listen address

--- docker/dev/bot.entrypoint.sh
+++ docker/dev/bot.entrypoint.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 set +x
 
-poetry run ./manage.py migrate && \
-poetry run ./manage.py collectstatic --noinput && \
-poetry run ./manage.py set_webhook && \
-gunicorn --workers=2 --env=DJANGO_SETTINGS_MODULE=server.settings server.wsgi :"${BOT_LISTEN_PORT}"
+echo 'migrate' && poetry run ./manage.py migrate && \
+echo 'collectstatistic' && poetry run ./manage.py collectstatic --noinput && \
+echo 'set_webhook' && poetry run ./manage.py set_webhook && \
+echo 'start app server' && gunicorn --workers=2 --env=DJANGO_SETTINGS_MODULE=server.settings --bind "${BOT_LISTEN_ADDR}:${BOT_LISTEN_PORT}" server.wsgi
