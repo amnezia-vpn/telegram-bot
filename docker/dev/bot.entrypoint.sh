7a739da08eab2d408dcdb7d4d9c616b9ac03d41f
Isaak Uchakaev <likid.geimfari@gmail.com>
2022-11-03 12:10:30 +0300

    Fix Dockerfile for bot

--- docker/dev/bot.entrypoint.sh
+++ docker/dev/bot.entrypoint.sh
@@ -2,5 +2,6 @@
 set +x
 
 poetry run ./manage.py migrate && \
+poetry run ./manage.py collectstatic --noinput && \
 poetry run ./manage.py set_webhook && \
-exec poetry run python manage.py runserver "${BOT_LISTEN_ADDR}:${BOT_LISTEN_PORT}"
+gunicorn --workers=2 --env=DJANGO_SETTINGS_MODULE=server.settings server.wsgi :"${BOT_LISTEN_PORT}"
