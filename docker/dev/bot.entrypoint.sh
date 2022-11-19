ee88f23f3eed7cddbc69b75e4b5aa420f2a234e5
Dmitry Danilov <dima.danilov9867@gmail.com>
2022-11-19 22:25:35 +0400

    Add key sync command to Docker entrypoint

--- docker/dev/bot.entrypoint.sh
+++ docker/dev/bot.entrypoint.sh
@@ -4,4 +4,5 @@ set +x
 echo 'migrate' && poetry run ./manage.py migrate && \
 echo 'collectstatistic' && poetry run ./manage.py collectstatic --noinput && \
 echo 'set_webhook' && poetry run ./manage.py set_webhook && \
+echo 'sync_with_vault' && poetry run ./manage.py sync_with_vault && \
 echo 'start app server' && gunicorn --workers=2 --env=DJANGO_SETTINGS_MODULE=server.settings --bind "${BOT_LISTEN_ADDR}:${BOT_LISTEN_PORT}" server.wsgi
